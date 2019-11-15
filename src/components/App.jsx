12d7a24d9aa11fccc19f23d127634f73d2f518b0
Devin Cooley <dcooley1350@gmail.com>
2019-11-15 08:52:55 -0800

    Scaffold project

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/globalStyles.css'
+
+import { Switch, Route } from 'react-router-dom';
+
+export function App(){
+  return(
+
+
+  );
+}
+
+export default App;
