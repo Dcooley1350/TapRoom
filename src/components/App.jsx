549ef6d0e3958ee96540045d09265a14f729f448
Devin Cooley <dcooley1350@gmail.com>
2019-11-15 10:17:43 -0800

    Write App component including switch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import '../css/globalStyles.css'
-
+import '../css/globalStyles.css';
+import Header from './Header';
+import Footer from './Footer';
+import Splash from './Splash';
+import Employee from './Employee';
+import Customer from './Customer';
 import { Switch, Route } from 'react-router-dom';
 
 export function App(){
   return(
-
-
+    <div>
+      <Header/>
+        <Switch>
+          <Route exact path='/' component={Splash}/>
+          <Route path='/Employee' component={Employee}/>
+          <Route exact path='/Customer' component={Customer}/>
+          <Route component={Error404} />
+        </Switch>
+      <Footer/>
+    </div>
   );
 }
 
